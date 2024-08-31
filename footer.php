4143b3041c2c801f4fc8bbc55bbba957b1f103ad
nusrat43 <nusratparween487@gmail.com>
2024-08-31 17:57:00 +0530

    Resolved merge conflict in footer.php

--- footer.php
+++ footer.php
@@ -1,102 +1,135 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-     <!-- Required meta tags -->
-     <meta charset="utf-8">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Jamia Rabbani</title>
     <!-- google fonts -->
     <link href="//fonts.googleapis.com/css2?family=Jost:wght@300;400;600;700&display=swap" rel="stylesheet">
     <!-- Template CSS -->
+
     <link rel="stylesheet" href="assets/css/style-starter.css">
+
+    <style>
+        .columns-2  img {
+            width: 100px;
+            height: 100px;
+        }
+        .columns-2 .two img{
+            width: 60px;
+            height: 60px;
+            margin: 5px;
+        }
+        .columns-2 .three img{
+            width: 70px;
+            height: 70px;
+            margin: 5px;
+        }
+        .image img{
+            margin-left: -20px ;
+        }
+    </style>
 </head>
+
 <body>
 
 
-  
- <!--/footer-->
- <footer class="w3l-footer-22 position-relative">
-    <div class="footer-sub py-5">
-        <div class="container py-md-5">
-            <div class="row sub-columns">
-                <div class="col-lg-4 col-md-6 sub-one-left ab-right-cont pr-lg-5 mb-md-0  mb-4">
-                    <h6>About </h6>
-                    <p>Lorem ipsum dolor sit amet elit. Velit beatae rem ullam dolore nisi esse quasi, sit amet.
-                        Lorem ipsum dolor sit amet elit.voluptatem accusantium
-                        doloremque.</p>
-                    <div class="columns-2">
-                        <ul class="social">
-                            <li><a href="#facebook"><span class="fab fa-facebook-f" aria-hidden="true"></span></a>
+
+    <!--/footer-->
+    <footer class="w3l-footer-22 position-relative">
+        <div class="footer-sub py-5">
+            <div class="container py-md-5">
+                <div class="row sub-columns">
+                    <div class="col-lg-4 col-md-6 sub-one-left ab-right-cont pr-lg-5 mb-md-0  mb-4">
+                        <h6>About </h6>
+                        <p>Jamia Rabbani Manorwa, founded by Maulana Syed Muhammad Sadiq, is renowned for its comprehensive Islamic curriculum and commitment to producing knowledgeable scholars.</p>
+                        <div class="columns-2">
+                            <div class="image">
+                                <a href="news.php" class="one"><img src="assets/images/news logo.png" alt=""></a>
+                               <a href="assets/images/books/ishtehar.pdf" class="two"><img src="assets/images/poster.png" alt=""></a>
+                               <a href="assets/images/books/apeel.pdf" class="three"><img src="assets/images/appeal.png" alt=""></a>
+                               <a href="calendar.php" class="one"><img src="assets/images/calendar.png" alt=""></a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-6 sub-two-right pl-lg-5">
+                        <h6>Useful Links</h6>
+                        <ul>
+                            <li><a href="index.php"><span class="fas fa-chevron-right mr-2"></span>Home</a>
                             </li>
-                            <li><a href="#linkedin"><span class="fab fa-linkedin-in" aria-hidden="true"></span></a>
+                            <li><a href="about.php"><span class="fas fa-chevron-right mr-2"></span>About</a>
                             </li>
-                            <li><a href="#twitter"><span class="fab fa-twitter" aria-hidden="true"></span></a>
-                            </li>
-                            <li><a href="#google"><span class="fab fa-google-plus-g" aria-hidden="true"></span></a>
-                            </li>
-                            <li><a href="#pinterest"><span class="fab fa-pinterest-p" aria-hidden="true"></span></a>
-                            </li>
+                            <li><a href="Department.php"><span class="fas fa-chevron-right mr-2"></span>Department</a></li>
+                            <li><a href="Virtual-Bookshelf.php"><span class="fas fa-chevron-right mr-2"></span>Virtual Bookshelf</a></li>
+                            <li><a href="Gallery.php"><span class="fas fa-chevron-right mr-2"></span>Gallery</a></li>
+                            <li><a href="Visitors.php"><span class="fas fa-chevron-right mr-2"></span>Visitors</a></li>
+                            <li><a href="contact.html"><span class="fas fa-chevron-right mr-2"></span>Contact</a></li>
                         </ul>
                     </div>
-                </div>
-                <div class="col-lg-2 col-md-6 sub-two-right pl-lg-5">
-                    <h6>Useful Links</h6>
-                    <ul>
-                        <li><a href="index.php"><span class="fas fa-chevron-right mr-2"></span>Home</a>
-                        </li>
-                        <li><a href="about.php"><span class="fas fa-chevron-right mr-2"></span>About</a>
-                        </li>
-                        <li><a href="Department.php"><span class="fas fa-chevron-right mr-2"></span>Courses</a></li>
-                        <li><a href="Virtual-Bookshelf.php"><span class="fas fa-chevron-right mr-2"></span>Courses</a></li>
-                        <li><a href="Gallery.php"><span class="fas fa-chevron-right mr-2"></span>Courses</a></li>
-                        <li><a href="Visitors.php"><span class="fas fa-chevron-right mr-2"></span>Courses</a></li>
-                        <li><a href="contact.html"><span class="fas fa-chevron-right mr-2"></span>Contact</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 sub-two-right pl-lg-5 mt-md-0 mt-4">
-                    <h6>Help & Support</h6>
-                    <ul>
-                        <li><a href="#live"><span class="fas fa-chevron-right mr-2"></span>Live
-                                Chart</a></li>
-                        <li><a href="#faq"><span class="fas fa-chevron-right mr-2"></span>Faq</a>
-                        </li>
-                        <li><a href="#support"><span class="fas fa-chevron-right mr-2"></span>Support</a>
-                        </li>
-                        <li><a href="#terms"><span class="fas fa-chevron-right mr-2"></span>Terms
-                                of Services</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 sub-two-right mt-md-0 mt-4">
-                    <h6>Research</h6>
-                    <ul>
-                        <li><a href="#"><span class="fas fa-chevron-right mr-2"></span> Academic Divisions</a>
-                        </li>
-                        <li><a href="#"><span class="fas fa-chevron-right mr-2"></span> Student Research</a>
-                        </li>
-                        <li><a href="#"><span class="fas fa-chevron-right mr-2"></span> Centers & Institutes</a>
-                        </li>
-                        <li><a href="#"><span class="fas fa-chevron-right mr-2"></span> Research Facilities</a></li>
-                    </ul>
+                    <div class="col-lg-3 col-md-6 sub-two-right pl-lg-5 mt-md-0 mt-4">
+                        <h6>Help & Support</h6>
+                        <ul>
+                            <li><a href="#live"><span class="fas fa-chevron-right mr-2"></span>Live
+                                    Chart</a></li>
+                            <li><a href="#faq"><span class="fas fa-chevron-right mr-2"></span>Faq</a>
+                            </li>
+                            <li><a href="#support"><span class="fas fa-chevron-right mr-2"></span>Support</a>
+                            </li>
+                            <li><a href="#terms"><span class="fas fa-chevron-right mr-2"></span>Terms
+                                    of Services</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sub-two-right mt-md-0 mt-4">
+                        <h6>Login For admin</h6>
+                        <form action="login.php" method=post>
+                            <div class="form-group">
+                                <input
+                                    type="text" name="username" id="username"
+                                    class="form-control border-0 py-4"
+                                    placeholder="Your userame"
+                                    required="required" />
+                            </div>
+                            <div class="form-group">
+                                <input
+                                    type="password" name="password" id="password"
+                                    class="form-control border-0 py-4"
+                                    placeholder="Your Password"
+                                    required="required" />
+                            </div>
+                            <div>
+                                <button
+                                    class="btn btn-primary btn-block border-0 py-3"
+                                    type="submit">
+                                    Submit Now
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+
+
                 </div>
             </div>
         </div>
-    </div>
-    </div>
-    <!-- copyright -->
-    <div class="copyright-footer text-center">
-        <div class="container">
-            <div class="columns">
-                <p>© 2021 Edukator. All rights reserved.Design by<a href="https://w3layouts.com/" target="_blank">
-                        W3layouts</a>
-                </p>
+        </div>
+        <!-- copyright -->
+        <div class="copyright-footer text-center">
+            <div class="container">
+                <div class="columns">
+                    <p>© 2021 Edukator. All rights reserved.Design by<a href="https://w3layouts.com/" target="_blank">
+                            W3layouts</a>
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- //copyright -->
-</footer>
-<!-- //footer -->
+        <!-- //copyright -->
+    </footer>
+    <!-- //footer -->
+
+
+
 
 
 
@@ -108,7 +141,7 @@
     </button>
     <script>
         // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function () {
+        window.onscroll = function() {
             scrollFunction()
         };
 
@@ -132,8 +165,8 @@
     <script src="assets/js/theme-change.js"></script>
     <!-- disable body scroll which navbar is in active -->
     <script>
-        $(function () {
-            $('.navbar-toggler').click(function () {
+        $(function() {
+            $('.navbar-toggler').click(function() {
                 $('body').toggleClass('noscroll');
             })
         });
@@ -142,7 +175,7 @@
 
     <!--/MENU-JS-->
     <script>
-        $(window).on("scroll", function () {
+        $(window).on("scroll", function() {
             var scroll = $(window).scrollTop();
 
             if (scroll >= 80) {
@@ -153,14 +186,14 @@
         });
 
         //Main navigation Active Class Add Remove
-        $(".navbar-toggler").on("click", function () {
+        $(".navbar-toggler").on("click", function() {
             $("header").toggleClass("active");
         });
-        $(document).on("ready", function () {
+        $(document).on("ready", function() {
             if ($(window).width() > 991) {
                 $("header").removeClass("active");
             }
-            $(window).on("resize", function () {
+            $(window).on("resize", function() {
                 if ($(window).width() > 991) {
                     $("header").removeClass("active");
                 }
@@ -171,4 +204,5 @@
     <script src="assets/js/bootstrap.min.js"></script>
 
 </body>
+
 </html>
